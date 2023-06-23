b371b6d2efe97213cce58af50247ef0429d67281
shenqing <shenqin1@msu.edu>
2023-06-23 13:52:06 -0400

    Practice of long tap gesture in swiftUI

---
+++ PracticeofSwiftUIIntermediate/LongPressGesturePractice.swift
@@ -0,0 +1,86 @@
+//
+//  LongPressGesturePractice.swift
+//  PracticeofSwiftUIIntermediate
+//
+//  Created by 沈清昊 on 6/23/23.
+//
+
+import SwiftUI
+
+struct LongPressGesturePractice: View {
+    @State var isComplete: Bool = false
+    @State var isSuccess: Bool  = false
+    var body: some View {
+        
+        VStack{
+            Rectangle()
+                .fill(isSuccess ? Color.green : Color.blue)
+                .frame(maxWidth: isComplete ? .infinity : 0)
+                .frame(height: 55)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .background(Color.gray)
+            
+            HStack {
+                Text("Click here")
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.black)
+                    .cornerRadius(10)
+                    .onLongPressGesture(minimumDuration: 1.0, maximumDistance: 50) {
+                        // at the min duration
+                        withAnimation(.easeInOut){
+                            isSuccess = true
+                        }
+                        
+                    } onPressingChanged: { isPressing in
+                        // start of press -> min duration
+                        if isPressing{
+                            withAnimation(.easeInOut(duration: 1.0)){
+                                isComplete = true
+                            }
+                        } else{
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                                if !isSuccess{
+                                    withAnimation(.easeInOut){
+                                        isComplete = false
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                
+                Text("Reset")
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.black)
+                    .cornerRadius(10)
+                    .onTapGesture {
+                        isComplete = false
+                        isSuccess = false
+                    }
+            }
+        }
+        
+        
+        
+        
+//        Text(isComplete ? "Completed" : "Not completed")
+//            .padding()
+//            .padding(.horizontal)
+//            .background(isComplete ? Color.green : Color.gray)
+//            .cornerRadius(10)
+////            .onTapGesture {
+////                isComplete.toggle()
+////            }
+//            .onLongPressGesture(minimumDuration: 5.0, maximumDistance: 50) {
+//                isComplete.toggle()
+//            }
+    }
+}
+
+struct LongPressGesturePractice_Previews: PreviewProvider {
+    static var previews: some View {
+        LongPressGesturePractice()
+    }
+}
